404ec4973ae100330ef75030dfd41a306ba625c8
Arvind S <arvind0598@gmail.com>
2019-11-17 10:11:16 +0530

    separated data and mixin

---
+++ pug.config.js
@@ -0,0 +1,9 @@
+const EXP_DATA = require('./src/data/experience.json');
+const SKILLS_DATA = require('./src/data/skills.json');
+
+module.exports = {
+  locals: {
+    EXP_DATA,
+    SKILLS_DATA,
+  },
+};
