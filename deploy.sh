c89b61f7027099d40d33473abe3122e564438a5a
Arvind Suresh <arvind0598@gmail.com>
2021-05-16 02:30:26 +0530

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git checkout year/2021
 touch build/CNAME
 echo "www.arvindsuresh.in" > build/CNAME
-git subtree split --prefix dist -b master
+git subtree split --prefix build -b master
 git push -f origin master:master
 git branch -D master
