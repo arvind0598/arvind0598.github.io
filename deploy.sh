63546ae980ac0cfc79e6fa9a068816ad3dddcd1f
Arvind Suresh <arvind0598@gmail.com>
2021-05-16 17:39:03 +0530

    updated deploy script to autocommit

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 git checkout year/2021
+touch build/.nojekyll
 touch build/CNAME
 echo "www.arvindsuresh.in" > build/CNAME
+git add build/*
+git commit -m "deploying production code"
 git subtree split --prefix build -b master
 git push -f origin master:master
 git branch -D master
