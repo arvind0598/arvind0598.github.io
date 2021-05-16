bad7d037a923a98e71f3719024bd83a689113ce1
Arvind Suresh <arvind0598@gmail.com>
2021-05-16 21:31:33 +0530

    fixed issues with script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ npm run build
 touch build/.nojekyll
 touch build/CNAME
 echo "www.arvindsuresh.in" > build/CNAME
-git add build/*
+git add build
 git commit -m "deploying production code"
 git subtree split --prefix build -b master
 git push -f origin master:master
