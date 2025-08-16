08ac3ac839a704f1a1b97aea6e43a7f2075ef3c2
Arvind Suresh <arvind0598@gmail.com>
2025-08-17 01:56:56 +0530

    use prettier, add public assets

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,6 @@ export default {
   resumeLink: "resume.pdf",
   linkedinLink: "https://linkedin.com/in/arvind0598",
   email: "mailto:hello@arvindsuresh.in",
+  smartstaffLink: "https://smartstaff.co.in",
+  blackRockLink: "https://www.blackrock.com/corporate",
 };
