af4d01fdb6cc1f45f7dc3bd637ba51b795a3b858
Arvind Suresh <arvind0598@gmail.com>
2025-08-23 01:26:21 +0530

    chore: minify content

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import compress from "astro-compress";
+
 export default defineConfig({
   site: "https://www.arvindsuresh.in",
-  integrations: [mdx()],
+  integrations: [mdx(), compress()],
   vite: {
     plugins: [tailwindcss()],
   },
