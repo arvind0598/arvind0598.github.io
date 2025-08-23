c78f531dbd8dd06e590a873b74320645824b115b
Arvind Suresh <arvind0598@gmail.com>
2025-08-24 00:02:09 +0530

    inline critical css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // astro.config.mjs
 import mdx from "@astrojs/mdx";
+import playformInline from "@playform/inline";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
-
 import compress from "astro-compress";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://www.arvindsuresh.in",
-  integrations: [mdx(), compress()],
+  integrations: [mdx(), compress(), playformInline()],
   vite: {
     plugins: [tailwindcss()],
   },
