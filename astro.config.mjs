696859ab26add87951f31d6187edcecf9f480f48
Arvind Suresh <arvind0598@gmail.com>
2025-08-24 12:15:59 +0530

    add gzip and brotli compression for static assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 import compress from "astro-compress";
 import { defineConfig } from "astro/config";
 
+import compressor from "astro-compressor";
+
 export default defineConfig({
   site: "https://www.arvindsuresh.in",
-  integrations: [mdx(), compress(), playformInline()],
+  integrations: [
+    mdx(),
+    compress(),
+    playformInline(),
+    compressor({ gzip: true, brotli: true }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
